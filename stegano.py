bd39e747831d8df797902b9fa89c27ee4a4326af
unknown <mamadououattara96@gmail.com>
2023-12-20 16:02:38 +0100

    adding the LSB1 modified

--- stegano.py
+++ stegano.py
@@ -1,9 +1,10 @@
 from PIL import Image
 import numpy
+import os
 
-image_path = "C:/Users/hakim/Documents/md5/aladin.jpg"
 
-
+image_path = "C:/Users/PC/Documents/md5/steganography/aladin.jpg"
+watermarked_image_path = "C:/Users/PC/Documents/md5/steganography/watermarked_image.png"
 
 def lsb_1(image_path, message):
 	image_object = Image.open(image_path)
@@ -24,5 +25,32 @@ def lsb_1(image_path, message):
 					break
 				index_binary_message += 1
 
+	watermarked_image_object = Image.fromarray(image_array, "RGB")
+	
+	root_path = os.path.split(image_path)[0]
+	watermarked_image_path = os.path.join(root_path, "watermarked_image.png")
+	watermarked_image_object.show()
+
+	watermarked_image_object.save(watermarked_image_path)
+
+
+
+def extract_message_from_image(watermarked_image_path):
+	watermarker_image_object = Image.open(watermarked_image_path)
+	watermarker_image_array = numpy.array(watermarker_image_object)
+
+	binary_message_array = watermarker_image_array % 2
+	binary_message_list = []
+	number_rows, number_cols, number_color = binary_message_array.shape
+	for index_row in range(0, number_rows):
+		for index_col in range(0, number_cols):
+			for index_color in range(0, number_color):
+				binary_message_list.append(str(binary_message_array[index_row, index_col, index_color]))
+				if binary_message_list[-8:] == ["0"]*8:
+					binary_message = "".join(binary_message_list)
+					message = ''.join([chr(int(binary_message[i : i+8],2)) for i in range(0, len(binary_message), 8)])
+					return message
+
 
 lsb_1(image_path, "Coucou les loulous")
+print(extract_message_from_image(watermarked_image_path))
